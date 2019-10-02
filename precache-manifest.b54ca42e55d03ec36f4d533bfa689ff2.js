5d5ce3a50fe1fbb6f37f23b7b1b975a57241a8ae
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-10-02 23:48:16 +0300

    Updates

---
+++ precache-manifest.b54ca42e55d03ec36f4d533bfa689ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf50b4222a21f079704e98d236b1a9",
+    "url": "/react-burger-builder-app/index.html"
+  },
+  {
+    "revision": "8e3845d763cc92d9a8a6",
+    "url": "/react-burger-builder-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "26c5cf26da73b7d568ff",
+    "url": "/react-burger-builder-app/static/js/2.7dd585c2.chunk.js"
+  },
+  {
+    "revision": "8e3845d763cc92d9a8a6",
+    "url": "/react-burger-builder-app/static/js/main.3858f801.chunk.js"
+  },
+  {
+    "revision": "8cedc990b0d379f3177f",
+    "url": "/react-burger-builder-app/static/js/runtime-main.a1b42b40.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-burger-builder-app/static/media/logo.25bf045c.svg"
+  }
+]);
